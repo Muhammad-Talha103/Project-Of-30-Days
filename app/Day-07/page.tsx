fd5968b05f63e72806d24a7ceab9a12be1785723
Muhammad Talha <talhagp908@gmail.com>
2024-10-01 19:03:41 -0230

    first commit

---
+++ app/Day-07/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import JokeGenerator from './component/joke-ganerater'
+
+
+export const page = () => {
+  return (
+    <div>
+        <JokeGenerator/>
+    </div>
+  )
+}
+
+export default page
