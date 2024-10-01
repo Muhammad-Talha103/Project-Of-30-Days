4bb6d22be5aded6aef491c7582259de435cef933
Muhammad Talha <talhagp908@gmail.com>
2024-10-01 14:50:07 -0230

    first commit

--- app/Day-06/page.tsx
+++ app/Day-06/page.tsx
@@ -5,10 +5,7 @@ import DigitalClock from '../Day-06/component/digital-clock/Digital-clock';
 export default function Home() {
   return (
     <div>
-      {/* <Head>
-        <title>Digital Clock</title>
-        <meta name="description" content="A simple digital clock using Next.js and Tailwind CSS" />
-      </Head> */}
+      
       <DigitalClock />
     </div>
   );
